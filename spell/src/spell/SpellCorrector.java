f60456b72b9be535e8138f1abd6fde55fd74bfa8
Lee <leesjensen@gmail.com>
2023-05-29 16:38:55 -0600

    impl(spell) tests

--- spell/src/spell/SpellCorrector.java
+++ spell/src/spell/SpellCorrector.java
@@ -5,21 +5,27 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class SpellCorrector implements ISpellCorrector {
+
+    private ITrie trie;
+
     @Override
     public void useDictionary(String dictionaryFileName) throws IOException {
+        trie = new Trie();
         File f = new File(dictionaryFileName);
         Scanner scanner = new Scanner(f);
 
-        StringBuilder sb = new StringBuilder();
-
         while (scanner.hasNext()) {
             String n = scanner.next();
-            sb.append(n);
+            trie.add(n);
         }
     }
 
     @Override
     public String suggestSimilarWord(String inputWord) {
-        return inputWord.toLowerCase();
+        inputWord = inputWord.toLowerCase();
+
+        var result = trie.find(inputWord);
+        System.out.println(result);
+        return inputWord;
     }
 }
