4577594787e2e749af88bcdcee62253311357b3a
Lee <lee.jensen@agilix.com>
2023-06-16 16:58:24 -0600

    impl(rook) first tests pass. need to handle pin

--- myChess/src/main/chess/Rook.java
+++ myChess/src/main/chess/Rook.java
@@ -12,10 +12,28 @@ public class Rook extends Piece {
     @Override
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition pos) {
         var moves = new HashSet<ChessMove>();
+        calculateMoves(board, pos, -1, 0, moves);
+        calculateMoves(board, pos, 1, 0, moves);
         calculateMoves(board, pos, 0, 1, moves);
         calculateMoves(board, pos, 0, -1, moves);
-        calculateMoves(board, pos, 1, 0, moves);
-        calculateMoves(board, pos, -1, 0, moves);
         return moves;
+    }
+
+    private void calculateMoves(ChessBoard board, ChessPosition pos, int rowInc, int colInc, HashSet<ChessMove> moves) {
+        int row = pos.getRow() + rowInc;
+        int col = pos.getColumn() + colInc;
+        while (row > 0 && col > 0 && row < 9 && col < 9) {
+            var newPos = new Position(row, col);
+            var pieceAt = board.getPiece(newPos);
+            if (pieceAt == null || pieceAt.getTeamColor() != pieceColor) {
+                moves.add(new Move(pos, newPos, null));
+                row += rowInc;
+                col += colInc;
+            }
+
+            if (pieceAt != null) {
+                break;
+            }
+        }
     }
 }
