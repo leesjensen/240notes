5e393520f130e2741b2f2bae5af884ecca397e9e
Lee <lee.jensen@agilix.com>
2023-08-03 09:48:47 -0600

    add(helloworld) Linus!

--- myChess/src/main/HelloWorld.java
+++ myChess/src/main/HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        var speaker = "James Gosling";
+        var speaker = "Linus Torvalds";
         var greeting = "%s says hello!";
         System.out.printf(greeting, speaker);
     }
