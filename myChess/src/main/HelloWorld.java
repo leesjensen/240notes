38bda282479d8c247f252f4a8638987ab1fd14df
Lee <lee.jensen@agilix.com>
2023-08-03 09:47:56 -0600

    add(helloworld) initial

---
+++ myChess/src/main/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        var speaker = "James Gosling";
+        var greeting = "%s says hello!";
+        System.out.printf(greeting, speaker);
+    }
+}
