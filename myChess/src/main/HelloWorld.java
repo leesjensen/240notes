658abf54c876f4d6f1d2f18d776c0d46c82802da
Lee <lee.jensen@agilix.com>
2023-08-03 10:38:26 -0600

    delete(helloworld) it was only an example for git

--- myChess/src/main/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-public class HelloWorld {
-    public static void main(String[] args) {
-        var speaker = "Linus Torvalds";
-        var greeting = "%s says hello!";
-        System.out.printf(greeting, speaker);
-    }
-}
