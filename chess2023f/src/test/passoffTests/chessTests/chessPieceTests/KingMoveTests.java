7401f3ad3768e9ab4a341138ddacc6e11d8f98e3
Lee <leesjensen@gmail.com>
2023-09-23 14:25:29 -0600

    enhance(movetests) simplified representation

--- chess2023f/src/test/passoffTests/chessTests/chessPieceTests/KingMoveTests.java
+++ chess2023f/src/test/passoffTests/chessTests/chessPieceTests/KingMoveTests.java
@@ -1,79 +1,61 @@
 package passoffTests.chessTests.chessPieceTests;
 
-import chess.*;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import passoffTests.TestFactory;
-
-import java.util.HashSet;
 
 public class KingMoveTests {
 
     @Test
     public void kingMoveUntilEdge() {
-        var board = TestFactory.loadBoard("""
-                | | | | | | | | |
-                | | | | | | | | |
-                | | | | | | | | |
-                | | | | | | | | |
-                | | | | | | | | |
-                | | | | | |K| | |
-                | | | | | | | | |
-                | | | | | | | | |
-                """);
-
-        var king = TestFactory.getNewPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.KING);
-        var position = TestFactory.getNewPosition(3, 6);
-        var validMoves = TestFactory.loadMoves(position, new int[][]{
-                {4, 6}, {4, 7}, {3, 7}, {2, 7}, {2, 6}, {2, 5}, {3, 5}, {4, 5},
-        });
-
-        Assertions.assertEquals(validMoves, king.pieceMoves(board, position), "Wrong moves");
+        TestFactory.validateMoves("""
+                        | | | | | | | | |
+                        | | | | | | | | |
+                        | | | | | | | | |
+                        | | | | | | | | |
+                        | | | | | | | | |
+                        | | | | | |K| | |
+                        | | | | | | | | |
+                        | | | | | | | | |
+                        """,
+                new int[]{3, 6},
+                new int[][]{{4, 6}, {4, 7}, {3, 7}, {2, 7}, {2, 6}, {2, 5}, {3, 5}, {4, 5}}
+        );
     }
 
 
     @Test
     public void kingCaptureEnemy() {
-        var board = TestFactory.loadBoard("""
-                | | | | | | | | |
-                | | | | | | | | |
-                | | | | | | | | |
-                | | | | | | | | |
-                | | | |N|n| | | |
-                | | | |k| | | | |
-                | | |P|b|p| | | |
-                | | | | | | | | |
-                """);
-
-        var king = TestFactory.getNewPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.KING);
-        var position = TestFactory.getNewPosition(3, 4);
-        var validMoves = TestFactory.loadMoves(position, new int[][]{
-                {4, 4}, {3, 5}, {2, 3}, {3, 3}, {4, 3},
-        });
-
-
-        Assertions.assertEquals(validMoves, king.pieceMoves(board, position), "Wrong moves");
+        TestFactory.validateMoves("""
+                        | | | | | | | | |
+                        | | | | | | | | |
+                        | | | | | | | | |
+                        | | | | | | | | |
+                        | | | |N|n| | | |
+                        | | | |k| | | | |
+                        | | |P|b|p| | | |
+                        | | | | | | | | |
+                        """,
+                new int[]{3, 4},
+                new int[][]{{4, 4}, {3, 5}, {2, 3}, {3, 3}, {4, 3}}
+        );
     }
 
 
     @Test
     public void kingBlocked() {
-        var board = TestFactory.loadBoard("""
-                | | | | | | |r|k|
-                | | | | | | |p|p|
-                | | | | | | | | |
-                | | | | | | | | |
-                | | | | | | | | |
-                | | | | | | | | |
-                | | | | | | | | |
-                | | | | | | | | |
-                """);
-
-        var king = TestFactory.getNewPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.KING);
-        var position = TestFactory.getNewPosition(8, 8);
-        var validMoves = new HashSet<ChessMove>();
-
-        Assertions.assertEquals(validMoves, king.pieceMoves(board, position), "Wrong moves");
+        TestFactory.validateMoves("""
+                        | | | | | | |r|k|
+                        | | | | | | |p|p|
+                        | | | | | | | | |
+                        | | | | | | | | |
+                        | | | | | | | | |
+                        | | | | | | | | |
+                        | | | | | | | | |
+                        | | | | | | | | |
+                        """,
+                new int[]{8, 8},
+                new int[][]{}
+        );
     }
 
 }
