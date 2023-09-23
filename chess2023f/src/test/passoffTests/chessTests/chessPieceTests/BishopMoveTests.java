d6669b9c4a4c8a798461bc0a13999c49387bff5f
Lee <leesjensen@gmail.com>
2023-09-23 10:38:13 -0600

    enhance(movetests) simplified representation

--- chess2023f/src/test/passoffTests/chessTests/chessPieceTests/BishopMoveTests.java
+++ chess2023f/src/test/passoffTests/chessTests/chessPieceTests/BishopMoveTests.java
@@ -1,84 +1,71 @@
 package passoffTests.chessTests.chessPieceTests;
 
-import chess.*;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import passoffTests.TestFactory;
-
-import java.util.HashSet;
 
 public class BishopMoveTests {
 
     @Test
     public void bishopMoveUntilEdge() {
-        var board = TestFactory.loadBoard("""
-                | | | | | | | | |
-                | | | | | | | | |
-                | | | | | | | | |
-                | | | |B| | | | |
-                | | | | | | | | |
-                | | | | | | | | |
-                | | | | | | | | |
-                | | | | | | | | |
-                """);
-
-        var bishop = TestFactory.getNewPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.BISHOP);
-        var position = TestFactory.getNewPosition(5, 4);
-        var validMoves = TestFactory.loadMoves(position, new int[][]{
-                {6, 5}, {7, 6}, {8, 7},
-                {4, 5}, {3, 6}, {2, 7}, {1, 8},
-                {4, 3}, {3, 2}, {2, 1},
-                {6, 3}, {7, 2}, {8, 1},
-        });
-
-        Assertions.assertEquals(validMoves, bishop.pieceMoves(board, position), "Wrong moves");
+        TestFactory.validateMoves("""
+                        | | | | | | | | |
+                        | | | | | | | | |
+                        | | | | | | | | |
+                        | | | |B| | | | |
+                        | | | | | | | | |
+                        | | | | | | | | |
+                        | | | | | | | | |
+                        | | | | | | | | |
+                        """,
+                new int[]{5, 4},
+                new int[][]{
+                        {6, 5}, {7, 6}, {8, 7},
+                        {4, 5}, {3, 6}, {2, 7}, {1, 8},
+                        {4, 3}, {3, 2}, {2, 1},
+                        {6, 3}, {7, 2}, {8, 1},
+                }
+        );
     }
 
 
     @Test
     public void bishopCaptureEnemy() {
-        var board = TestFactory.loadBoard("""
-                | | | | | | | | |
-                | | | |Q| | | | |
-                | | | | | | | | |
-                | |b| | | | | | |
-                |r| | | | | | | |
-                | | | | | | | | |
-                | | | | |P| | | |
-                | | | | | | | | |
-                """);
-
-        var bishop = TestFactory.getNewPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.BISHOP);
-        var position = TestFactory.getNewPosition(5, 2);
-        var validMoves = TestFactory.loadMoves(position, new int[][]{
-                {6, 3}, {7, 4},
-                {4, 3}, {3, 4}, {2, 5},
-                // none
-                {6, 1},
-        });
-
-        Assertions.assertEquals(validMoves, bishop.pieceMoves(board, position), "Wrong moves");
+        TestFactory.validateMoves("""
+                        | | | | | | | | |
+                        | | | |Q| | | | |
+                        | | | | | | | | |
+                        | |b| | | | | | |
+                        |r| | | | | | | |
+                        | | | | | | | | |
+                        | | | | |P| | | |
+                        | | | | | | | | |
+                        """,
+                new int[]{5, 2},
+                new int[][]{
+                        {6, 3}, {7, 4},
+                        {4, 3}, {3, 4}, {2, 5},
+                        // none
+                        {6, 1},
+                }
+        );
     }
 
 
     @Test
     public void bishopBlocked() {
-        var board = TestFactory.loadBoard("""
-                | | | | | | | | |
-                | | | | | | | | |
-                | | | | | | | | |
-                | | | | | | | | |
-                | | | | | | | | |
-                | | | | | | | | |
-                | | | | |R| |P| |
-                | | | | | |B| | |
-                """);
-
-        var bishop = TestFactory.getNewPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.BISHOP);
-        var position = TestFactory.getNewPosition(1, 6);
-        var validMoves = new HashSet<ChessMove>();
-
-        Assertions.assertEquals(validMoves, bishop.pieceMoves(board, position), "Wrong moves");
+        TestFactory.validateMoves("""
+                        | | | | | | | | |
+                        | | | | | | | | |
+                        | | | | | | | | |
+                        | | | | | | | | |
+                        | | | | | | | | |
+                        | | | | | | | | |
+                        | | | | |R| |P| |
+                        | | | | | |B| | |
+                        """,
+                new int[]{1, 6},
+                new int[][]{}
+        );
     }
 
 }
