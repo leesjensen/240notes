9a22b6caf8fa590dad17ad1a484c714607ac8af6
Lee <leesjensen@gmail.com>
2023-09-25 13:20:48 -0600

    moved code examples from helloworld to classExamples

--- helloworld/example/src/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-public class HelloWorld {
-    public static void main(String[] args) {
-        var speaker = "James Gosling";
-        var greeting = "%s says hello!";
-        System.out.printf(greeting, speaker);
-    }
-
-}
