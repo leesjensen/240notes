6ae43f937493ce279b396fa07544d7da52366435
Lee <lee.jensen@agilix.com>
2023-09-27 13:34:20 -0600

    enhance(generics) with examples

---
+++ classExamples/src/generics/GenericExample.java
@@ -0,0 +1,18 @@
+package generics;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GenericExample {
+    static class Storage<T> {
+        List<T> items = new ArrayList<>();
+
+        void add(T item) {
+            items.add(item);
+        }
+
+        T get(int i) {
+            return items.get(i);
+        }
+    }
+}
