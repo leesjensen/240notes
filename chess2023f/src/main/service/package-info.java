c8d215728dd58c11f3e9248f57d971040280e7aa
lee <leesjensen@gmail.com>
2023-10-07 16:13:05 -0600

    added(service) basic stub and docs

---
+++ chess2023f/src/main/service/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * The service package contains the Chess service endpoint implementations. This
+ * includes providing methods for the HTTP handlers to call, and pushing application
+ * data into the persistence layer.
+ * <p>
+ * Three services are provided. The UserService, GameService, and AuthService.
+ */
+package service;
