c99af8ae230122e9f132a55971a63fe52f3fa283
Lee <lee.jensen@agilix.com>
2023-10-30 16:43:49 -0600

    impl(chess phase 3) admin service test

--- chess2023f/server/src/test/serviceTests/AdminServiceTests.java
+++ chess2023f/server/src/test/serviceTests/AdminServiceTests.java
@@ -1,29 +1,32 @@
 package serviceTests;
 
+import dataAccess.DataAccessException;
 import dataAccess.MemoryDataAccess;
 import model.UserData;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import service.CodedException;
-import service.UserService;
+import service.*;
 
 public class AdminServiceTests {
 
     @Test
-    public void registerUser() {
-        var service = new UserService(new MemoryDataAccess());
+    public void clear() throws Exception {
+        var memoryDataAccess = new MemoryDataAccess();
+        var userService = new UserService(memoryDataAccess);
         var user = new UserData("juan", "too many secrets", "juan@byu.edu");
+        var authData = userService.registerUser(user);
 
-        Assertions.assertDoesNotThrow(() -> service.registerUser(user));
-    }
+        var gameService = new GameService(memoryDataAccess);
+        gameService.createGame("testGame");
 
+        var service = new AdminService(memoryDataAccess);
+        Assertions.assertDoesNotThrow(service::clearApplication);
 
-    @Test
-    public void registerUserDuplicate() {
-        var service = new UserService(new MemoryDataAccess());
-        var user = new UserData("juan", "too many secrets", "juan@byu.edu");
+        var authService = new AuthService(memoryDataAccess);
+        Assertions.assertThrows(CodedException.class, () -> authService.createSession(user));
+        Assertions.assertThrows(CodedException.class, () -> authService.getAuthData(authData.authToken()));
 
-        Assertions.assertDoesNotThrow(() -> service.registerUser(user));
-        Assertions.assertThrows(CodedException.class, () -> service.registerUser(user));
+        var games = gameService.listGames();
+        Assertions.assertEquals(0, games.size());
     }
 }
