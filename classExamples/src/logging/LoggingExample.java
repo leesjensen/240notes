309a565d028d3071a9a9514fc9135643d3d2bede
Lee <leesjensen@gmail.com>
2023-10-31 14:20:15 -0600

    enhance(logging) make logger singleton

--- classExamples/src/logging/LoggingExample.java
+++ classExamples/src/logging/LoggingExample.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import java.util.logging.*;
 
 public class LoggingExample {
-    static Logger logger = Logger.getLogger("myLogger");
+    public static Logger logger = Logger.getLogger("myLogger");
 
     public static void main(String[] args) throws IOException {
 
