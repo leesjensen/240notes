6a990996f0254e7a683e15a75fdb88b28b39dcd1
Lee <leesjensen@gmail.com>
2023-11-06 09:21:55 -0700

    enhance(console) examples to clarify sequences

--- classExamples/src/console/FancyCalculatorExample.java
+++ classExamples/src/console/FancyCalculatorExample.java
@@ -1,25 +1,15 @@
 package console;
 
 public class FancyCalculatorExample {
-
-    private static final String SET_TEXT_COLOR = "\u001b[38;5;";
-    private static final String SET_BG_COLOR = "\u001b[48;5;";
-
-    public static final String MAGENTA_TEXT = SET_TEXT_COLOR + "5m";
-
-    public static final String GREY_BG = SET_BG_COLOR + "242m";
-    public static final String WHITE_BG = SET_BG_COLOR + "15m";
-
     public static void main(String[] args) {
         int result = 0;
         for (var arg : args) {
             result += Integer.parseInt(arg);
         }
 
-        System.out.print(MAGENTA_TEXT);
-        System.out.print(GREY_BG);
-        System.out.printf("%s = ", String.join(" + ", args));
-        System.out.print(WHITE_BG);
+        System.out.print("\u001b[35;100m");
+        System.out.printf(" %s = ", String.join(" + ", args));
+        System.out.print("\u001b[107m");
         System.out.printf(" %d ", result);
     }
 }
--- classExamples/src/console/InteractiveCalculatorExample.java
+++ classExamples/src/console/InteractiveCalculatorExample.java
@@ -4,16 +4,18 @@ import java.util.Scanner;
 
 public class InteractiveCalculatorExample {
     public static void main(String[] args) throws Exception {
-        System.out.printf("Type your numbers%n>>> ");
-        Scanner scanner = new Scanner(System.in);
-        String line = scanner.nextLine();
-        var numbers = line.split(" ");
+        while (true) {
+            System.out.printf("Type your numbers%n>>> ");
+            Scanner scanner = new Scanner(System.in);
+            String line = scanner.nextLine();
+            var numbers = line.split(" ");
 
-        int result = 0;
-        for (var number : numbers) {
-            result += Integer.parseInt(number);
+            int result = 0;
+            for (var number : numbers) {
+                result += Integer.parseInt(number);
+            }
+            var equation = String.join(" + ", numbers);
+            System.out.printf("%s = %d%n", equation, result);
         }
-        var equation = String.join(" + ", numbers);
-        System.out.printf("%s = %d%n", equation, result);
     }
 }
