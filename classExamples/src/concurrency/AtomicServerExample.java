dcf129c9f1a6a32cc249e16bcbd67d67d84d695f
Lee <lee.jensen@agilix.com>
2023-11-27 16:45:08 -0700

    enhanced(concurrency) with more examples

--- classExamples/src/concurrency/AtomicServerExample.java
+++ classExamples/src/concurrency/AtomicServerExample.java
@@ -2,22 +2,22 @@ package concurrency;
 
 import spark.Spark;
 
-import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
 
 
-//    while true; do curl localhost:8080/toggle; print "\n"; done &
-//    while true; do curl localhost:8080/toggle; print "\n"; done &
+//    while true; do curl localhost:8080/add/1; print "\n"; done &
+//    while true; do curl localhost:8080/add/-1; print "\n"; done &
 //    wait
 
 public class AtomicServerExample {
-    static AtomicBoolean value = new AtomicBoolean(true);
+    static AtomicInteger sum = new AtomicInteger(0);
 
     public static void main(String[] args) {
         Spark.port(8080);
-        Spark.get("/toggle", (req, res) -> {
-            value.compareAndExchange(true, false);
-            var v = value.compareAndExchange(false, true);
-            return " " + (v == false);
+        Spark.get("/add/:value", (req, res) -> {
+            var value = Integer.parseInt(req.params(":value"));
+            value = sum.addAndGet(value);
+            return " " + value;
         });
     }
 }
--- classExamples/src/concurrency/SynchronizedMultithreadedServerExample.java
+++ classExamples/src/concurrency/SynchronizedMultithreadedServerExample.java
@@ -7,18 +7,18 @@ import spark.Spark;
 //    while true; do curl localhost:8080/add/-1; print "\n"; done &
 //    wait
 
-public class MultithreadedServerExample {
+public class SynchronizedMultithreadedServerExample {
     static int sum = 0;
-//    static Object lock = new Object();
+    static Object lock = new Object();
 
     public static void main(String[] args) {
         Spark.port(8080);
         Spark.get("/add/:value", (req, res) -> {
-            //           synchronized (lock) {
-            var value = Integer.parseInt(req.params(":value"));
-            sum += value;
-            return " " + sum;
-            //          }
+            synchronized (lock) {
+                var value = Integer.parseInt(req.params(":value"));
+                sum += value;
+                return " " + sum;
+            }
         });
     }
 }
