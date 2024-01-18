f74e57b5f16b84d9aa198f1f659afd1b63f7a815
Lee <lee.jensen@agilix.com>
2024-01-18 14:01:44 -0700

    add(inheritance) example

--- classExamples/src/fundamentals/InheritanceExample.java
+++ classExamples/src/fundamentals/InheritanceExample.java
@@ -2,28 +2,28 @@ package fundamentals;
 
 public class InheritanceExample {
     public static void main(String[] args) {
-        var sub = new SuperClass();
+        var sub = new SubClass();
         System.out.println(sub);
-        System.out.println((SubClass) sub);
+        System.out.println((SuperClass) sub);
         System.out.println(sub.SuperToString());
     }
 
-    public static class SubClass {
-        String name = "base";
+    public static class SuperClass {
+        String name = "super";
 
         public String getName() {
             return name;
         }
     }
 
-    public static class SuperClass extends SubClass {
+    public static class SubClass extends SuperClass {
 
         public String SuperToString() {
             return super.toString();
         }
 
         public String toString() {
-            return "Derived of " + getName();
+            return "Sub-class of " + getName();
         }
     }
 }
