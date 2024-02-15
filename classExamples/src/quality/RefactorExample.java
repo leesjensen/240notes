13e9df1399917b31f43e85330e1b539a922db9b8
Lee <lee.jensen@agilix.com>
2024-02-15 08:59:25 -0700

    enhance(quality) refactor test

--- classExamples/src/quality/RefactorExample.java
+++ classExamples/src/quality/RefactorExample.java
@@ -6,8 +6,9 @@ public class RefactorExample {
     public static void main(String[] args) {
         // Here is our test
         for (var i : Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7)) {
-            System.out.println(originalFibonacci(i));
-            System.out.println(refactoredFibonacci(i));
+            if (originalFibonacci(i) != refactoredFibonacci(i)) {
+                System.out.println("Wrong result");
+            }
         }
     }
 
