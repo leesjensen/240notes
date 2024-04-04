d458f8f324fd1e91aa0a751bce46035ef5acc861
Lee <lee.jensen@agilix.com>
2024-04-04 13:02:39 -0600

    enhance(concurrent) example

--- classExamples/src/concurrency/ThreadExample.java
+++ classExamples/src/concurrency/ThreadExample.java
@@ -4,20 +4,25 @@ public class ThreadExample {
 
     public static void main(String[] args) {
 
-        new CountingThread().start();
-        new CountingThread().start();
+        new CountingThread("1").start();
+        new CountingThread("2").start();
 
         System.out.println("\nExit Main Thread");
     }
 
 
     static class CountingThread extends Thread {
+        String name;
+
+        public CountingThread(String name) {
+            this.name = name;
+        }
+
         public void run() {
-            var id = this.threadId();
             for (int i = 0; i != 10; i++) {
-                System.out.printf("%s:%d ", id, i);
+                System.out.printf("%s:%d ", name, i);
             }
-            System.out.printf("%nExit thread %s%n", id);
+            System.out.printf("%nExit thread %s%n", name);
         }
     }
 }
