cd15b87d9b482598bc3ee8ed94eb553181a46071
Lee <leesjensen@gmail.com>
2024-09-18 09:49:43 -0600

    enhance(exception) examples

--- classExamples/src/fundamentals/ExceptionExample.java
+++ classExamples/src/fundamentals/ExceptionExample.java
@@ -6,24 +6,24 @@ import java.io.FileNotFoundException;
 public class ExceptionExample {
     public static void main(String[] args) {
         // No need to explicitly handle unchecked exceptions.
-        unChecked();
+        //unChecked();
 
         // Exceptions are handled centrally for anything that happens in this scope.
         try {
             var example = new ExceptionExample();
             example.loadConfig();
         } catch (FileNotFoundException ex) {
-            System.out.printf("Required file not found: %s", ex);
+            System.out.printf("Required file not found: %s%n", ex);
         } catch (Exception ex) {
-            System.out.printf("General error: %s", ex);
+            System.out.printf("General error: %s%n", ex);
         } finally {
             System.out.println("Program completed");
         }
     }
 
     private void loadConfig() throws Exception {
-        loadConfigFile("user");
-        loadConfigFile("system");
+        loadConfigFile("user.config.json");
+        loadConfigFile("system.config.json");
     }
 
     // Note that the function indicates that it can throw an exception.
@@ -31,7 +31,7 @@ public class ExceptionExample {
         var file = new File(location);
         if (!file.exists()) {
             // Let the code above know there was an exception.
-            throw new FileNotFoundException();
+            throw new FileNotFoundException("couldn't find: " + location);
         }
 
         // Otherwise load the configuration
