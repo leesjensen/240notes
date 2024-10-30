5465ee669a832351abb16649ed7bab4a7b891f57
Lee <leesjensen@gmail.com>
2024-10-30 13:34:50 -0600

    enhance(httpclient) with post example

--- classExamples/src/webapi/ClientWithBodyExample.java
+++ classExamples/src/webapi/ClientWithBodyExample.java
@@ -11,9 +11,9 @@ import java.util.Map;
 public class ClientWithBodyExample {
     public static void main(String[] args) throws Exception {
         // Specify the desired endpoint
-        URI uri = new URI("http://localhost:8080/echo");
+        URI uri = new URI("http://localhost:8080/name");
         HttpURLConnection http = (HttpURLConnection) uri.toURL().openConnection();
-        http.setRequestMethod("POST");
+        http.setRequestMethod("PUT");
 
         // Specify that we are going to write out data
         http.setDoOutput(true);
@@ -22,7 +22,7 @@ public class ClientWithBodyExample {
         http.addRequestProperty("Content-Type", "application/json");
 
         // Write out the body
-        var body = Map.of("name", "joe", "type", "cat");
+        var body = Map.of("bud", "joe", "sue", "tom");
         try (var outputStream = http.getOutputStream()) {
             var jsonBody = new Gson().toJson(body);
             outputStream.write(jsonBody.getBytes());
--- classExamples/src/webapi/ServerExample.java
+++ classExamples/src/webapi/ServerExample.java
@@ -21,6 +21,7 @@ public class ServerExample {
 
         // Register handlers for each endpoint using the method reference syntax
         Spark.post("/name/:name", this::addName);
+        Spark.put("/name", this::updateName);
         Spark.get("/name", this::listNames);
         Spark.delete("/name/:name", this::deleteName);
 
@@ -29,6 +30,15 @@ public class ServerExample {
 
     private Object addName(Request req, Response res) {
         names.add(req.params(":name"));
+        return listNames(req, res);
+    }
+
+    private Object updateName(Request req, Response res) {
+        HashMap<String, String> updateNameMap = getBody(req, new HashMap<String, String>().getClass());
+        for (Map.Entry<String, String> updateEntry : updateNameMap.entrySet()) {
+            names.remove(updateEntry.getKey());
+            names.add(updateEntry.getValue());
+        }
         return listNames(req, res);
     }
 
@@ -41,5 +51,15 @@ public class ServerExample {
     private Object deleteName(Request req, Response res) {
         names.remove(req.params(":name"));
         return listNames(req, res);
+    }
+
+
+    private static <T> T getBody(Request request, Class<T> clazz) {
+        var bodyText = request.body();
+        var body = new Gson().fromJson(bodyText, clazz);
+        if (body == null) {
+            throw new RuntimeException("missing required body");
+        }
+        return body;
     }
 }
